e5002e5eb7ba6ce9c565c61352e9451dad2e0ceb
catsbi01 <167311867+catsbi01@users.noreply.github.com>
2024-04-18 15:48:12 +0100

    Add files via upload

---
+++ P09/Color.h
@@ -0,0 +1,17 @@
+#ifndef color_h
+#define color_h
+
+class Color{
+    public:
+        Color(unsigned char red, unsigned char green, unsigned char blue);
+        Color(const Color& c);
+        unsigned char red() const;
+        unsigned char green() const;
+        unsigned char blue() const;
+        static const Color RED, GREEN, BLUE, BLACK, WHITE;
+        bool equal_to(const Color& other) const;
+        void invert();
+    private:
+        unsigned char r, g, b;
+};
+#endif
